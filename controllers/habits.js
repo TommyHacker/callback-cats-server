a7c9a72f81242120655efa2155db6551eac00e83
TommyHacker <t.hacker.webdev@gmail.com>
2022-06-29 14:48:52 +0100

    Revert "refactor"
    This reverts commit 9b9787ed7eb06b32f3aa354438d605e7e3368acf.

--- controllers/habits.js
+++ controllers/habits.js
@@ -22,7 +22,7 @@ async function createHabit(req, res) {
       message: `Type ${habitType} habit successfully created for user ${userId}`,
     });
   } catch (err) {
-    res.status(422).json(err.message);
+    res.status(422).json({ err });
   }
 }
 
