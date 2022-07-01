84be2e05c9475995ed7638bd510d2ad99fec1491
TommyHacker <t.hacker.webdev@gmail.com>
2022-07-01 01:53:00 +0100

    can now delete habits

--- controllers/habits.js
+++ controllers/habits.js
@@ -69,6 +69,8 @@ async function createHabit(req, res) {
 // }
 
 async function deleteHabit(req, res) {
+  console.log("user id", res.locals.currentUser.id);
+  console.log("habit id", req.params.id);
   try {
     const userId = res.locals.currentUser.id;
     const habitId = req.params.id;
@@ -77,7 +79,7 @@ async function deleteHabit(req, res) {
 
     // filter through all habits, return them all into a new array EXCEPT if id === chosen id;
     const newHabits = await user.habits.filter((habit) => {
-      if (habit._id !== habitId) return habit;
+      if (habit._id != habitId) return habit;
     });
     // replace user.habits with the new array and save
     user.habits = [...newHabits];
